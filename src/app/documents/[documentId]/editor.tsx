c102826ea701013f42a2df806ed1c0a1b8b158fd
VasilyGorbunov <vasily.gorbunov@gmail.com>
2025-08-07 21:32:06 +0300

    2:19:00

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import TableRow from "@tiptap/extension-table-row";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
 import Underline from "@tiptap/extension-underline";
+import FontFamily from "@tiptap/extension-font-family";
+import TextStyle from "@tiptap/extension-text-style";
 
 import { useEditorStore } from "@/store/use-editor-store";
 
@@ -66,6 +68,8 @@ export const Editor = () => {
         levels: [1, 2, 3, 4, 5, 6],
       }),
       Underline,
+      FontFamily,
+      TextStyle,
     ],
     content: `
         <table>
